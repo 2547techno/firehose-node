f70ceebcccb46919cf433c3f389eaf40094429ba
2547techno <technowizz2547@gmail.com>
2023-07-27 01:53:20 -0400

    express

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
 import { Connection } from "./lib/connections";
 import { IRCMessage } from "irc-message-ts";
+import Express, { Request, Response, json } from "express";
 
+const app = Express();
+const PORT = process.env.PORT ?? 3001;
 const connections: Map<string, Connection> = new Map();
 
-["drdisrespect", "2547techno", "tmiloadtesting2"].forEach((channel) => {
-    const conn = new Connection(channel);
+const middleware = [json()];
 
-    conn.addListener("close", () => {
-        console.log("CLOSE:", channel);
-        connections.delete(channel);
-    });
+app.post("/channels", middleware, (req: Request, res: Response) => {
+    const channels: string[] = req.body.channels;
+    if (!channels) {
+        return res.status(400).json({
+            message: "missing `channels` field",
+        });
+    }
 
-    conn.addListener("PRIVMSG", (message: IRCMessage) => {
-        // console.log(new Date().toLocaleString(), message.raw, "\n");
-    });
+    for (const channel of channels) {
+        if (connections.has(channel)) continue;
 
-    connections.set(channel, conn);
+        const conn = new Connection(channel);
+
+        conn.addListener("close", () => {
+            console.log("CLOSE:", channel);
+            connections.delete(channel);
+        });
+
+        conn.addListener("PRIVMSG", (message: IRCMessage) => {
+            // console.log(new Date().toLocaleString(), message.raw, "\n");
+        });
+
+        connections.set(channel, conn);
+    }
+
+    res.send();
+});
+
+app.listen(PORT, () => {
+    console.log(`[EXPRESS] Listening on ${PORT}`);
 });
