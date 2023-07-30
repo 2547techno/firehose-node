5a39be4244cb22f49a979ff082fdaa2e760bcc00
2547techno <technowizz2547@gmail.com>
2023-07-30 04:53:56 -0400

    update channels from delegation messages

--- src/amqp.ts
+++ src/amqp.ts
@@ -1,18 +1,66 @@
 import amqplib from "amqplib";
 import { config } from "./lib/config";
 import { EventEmitter } from "events";
-const QUEUE_NAME = config.amqp.queueName;
+import assert from "assert";
+import { firehoseChannels } from "./lib/streams";
+const MESSAGE_QUEUE_NAME = config.amqp.queueNames.messageQueue;
+const DELEGATION_QUEUE_NAME = config.amqp.queueNames.delegationQueue;
 export const messageEvent = new EventEmitter();
+
+type Delegation = {
+    id: string;
+    channelNames: string[];
+};
 
 export async function initAMQP() {
     const conn = await amqplib.connect(
         `amqp://${config.amqp.user}:${config.amqp.password}@${config.amqp.url}`
     );
 
+    console.log("[AMQP] Connected to server");
+
     const channel = await conn.createChannel();
-    await channel.assertQueue(QUEUE_NAME);
+    await channel.assertQueue(MESSAGE_QUEUE_NAME);
+    console.log("[AMQP] Connected to queue", MESSAGE_QUEUE_NAME);
 
     messageEvent.on("message", (message: string) => {
-        channel.sendToQueue(QUEUE_NAME, Buffer.from(message));
+        channel.sendToQueue(MESSAGE_QUEUE_NAME, Buffer.from(message));
     });
+
+    if (!process.env.STANDALONE_LIST) {
+        assert.notStrictEqual(
+            process.env.NODE_ID,
+            undefined,
+            "NODE_ID needs to be defined when not in STANDALONE mode!"
+        );
+
+        await channel.assertQueue(DELEGATION_QUEUE_NAME);
+        console.log("[AMQP] Connected to queue", DELEGATION_QUEUE_NAME);
+
+        channel.consume(DELEGATION_QUEUE_NAME, (message) => {
+            if (!message) return;
+
+            try {
+                const delegations: Delegation[] = JSON.parse(
+                    message.content.toString()
+                );
+                channel.ack(message);
+                updateFromDelegation(delegations);
+            } catch (err) {
+                console.log("[DELEGATION] Cannot parse delegation message");
+            }
+        });
+    }
+}
+
+function updateFromDelegation(delegations: Delegation[]) {
+    for (const delegation of delegations) {
+        if (delegation.id === process.env.NODE_ID) {
+            console.log("[DELEGATION] Updating channels from delegation");
+            firehoseChannels.clear();
+            for (const channelName of delegation.channelNames) {
+                firehoseChannels.add(channelName);
+            }
+        }
+    }
 }
